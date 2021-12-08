11b8fdf4ae2341d760800c32a80bc05ab9e51565
Ricky Davenport <ricky.davenport@wwt.com>
2021-12-08 10:58:46 -0600

    add initial fastapi scaffolding

---
+++ engine/src/engine/main.py
@@ -0,0 +1,35 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+ORIGINS = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.get('/')
+@app.head('/')
+async def heartbeat():
+    return 'Engine is up and running!'
+
+
+def main():
+    uvicorn.run(
+        'engine.main:app',
+        port=3005,
+        host='0.0.0.0',
+        reload=True,
+        proxy_headers=True,
+        forwarded_allow_ips='*'
+    )
+
+
+if __name__ == '__main__':
+    main()
