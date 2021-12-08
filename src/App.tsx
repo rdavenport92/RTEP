d69de33e04b16bfbcc702c4291622f4de22515cf
Ricky Davenport <ricky.davenport@wwt.com>
2021-12-08 11:00:29 -0600

    add initial ui scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.scss';
 
-const App: React.FC = () => <div className="hello-container">Hello there!</div>;
+const testGet = async (updateState: (data: string) => void) => {
+  let updateValue = '';
+
+  try {
+    const result = await axios.get('http://localhost:3005/');
+    updateValue = result.data;
+  } catch (err) {
+    updateValue = (err as Error).message;
+  } finally {
+    updateState(updateValue);
+  }
+};
+
+const App: React.FC = () => {
+  const [state, setState] = useState('Click the button to check status.');
+
+  return (
+    <div className="hello-container">
+      <p>{state}</p>
+      <button onClick={() => testGet(setState)}>Check Engine Status</button>
+    </div>
+  );
+};
 
 export default App;
